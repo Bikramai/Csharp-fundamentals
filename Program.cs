445a2ac7472487baa04896b031de78ee66d949a5
bikram <bikra@BIKRAM-LENOVO>
2025-03-02 12:08:08 -0500

    Control Flow -Iterations -While loops

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,18 @@ namespace Control_Flow
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("Type Your Name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+                break;
+            }
         }
     }
 }
