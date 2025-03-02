0e921b1589b2952ac8a0130b209c68e7afd7d4d6
bikram <bikra@BIKRAM-LENOVO>
2025-03-02 12:37:48 -0500

    Random class -random next-chars(ASCII)

--- Program.cs
+++ Program.cs
@@ -11,11 +11,13 @@ namespace Random_Class
     {
         static void Main(string[] args)
         {
-            var random = new Random();
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(random.Next(1, 10));
-            }
+            //var random = new Random();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(random.Next(1, 10));
+            //}
+
+            Console.WriteLine((int)'a');
         }
     }
 }
