7d55810d15ffbc7c78b1b903df3c765c7a52da49
bikram <bikra@BIKRAM-LENOVO>
2025-03-02 14:25:24 -0500

    Arrays - Length, IndexOf, Clear, Copy, Sort, Reverse

--- Program.cs
+++ Program.cs
@@ -5,24 +5,49 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Random_Class
+namespace Arrays
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var random = new Random();
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
 
-            const int passwordLength = 10;
+            // Length
+            Console.WriteLine("Length: " + numbers.Length);
 
-            var buffer = new char[passwordLength];
-            for (var i = 0; i < passwordLength; i++)
-                buffer[i] = (char)('a'  + random.Next(0, 26));
+            // IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
 
+            // Clear()
+            Array.Clear(numbers, 0, 2);
 
-            var password = new string(buffer);
-            Console.WriteLine(password);
-            
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            // Copy()
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+                Console.WriteLine(n);
+
+            // Sort()
+            Array.Sort(numbers);
+
+            Console.WriteLine("Effect of Sort()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            // Reverse()
+            Array.Reverse(numbers);
+
+            Console.WriteLine("Effect of Reverse()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
         }
     }
 }
