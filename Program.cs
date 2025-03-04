2c3268294254edf45a651da92a5244d635eb6bb6
bikram <bikra@BIKRAM-LENOVO>
2025-03-04 11:24:59 -0500

    Live Coding: Summarizing Text

--- Program.cs
+++ Program.cs
@@ -11,31 +11,12 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            var fullName = "Bikram Phurumbo ";
-            Console.WriteLine("My Full Name is: '{0}'", fullName.Trim());
-            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
-
-            var index = fullName.IndexOf(' ');
-            var firstName = fullName.Substring(0, index);
-            var lastName = fullName.Substring(index + 1);
-            Console.WriteLine("First Name: " + firstName);
-            Console.WriteLine("Last Name: " + lastName);
-
-            var names = fullName.Split(' ');
-            Console.WriteLine("First Name: " + names[0]);
-            Console.WriteLine("Last Name: " + names[1]);
-
-            Console.WriteLine(fullName.Replace("Bikram", "Bike"));
-
-            if (String.IsNullOrWhiteSpace(" "))
-                Console.WriteLine("Invalid");
-
-            var str = "25";
-            var age = Convert.ToByte(str);
-            Console.WriteLine(age);
-
-            float price = 29.95f;
-            Console.WriteLine(price.ToString("C0"));
+            var sentence = "This is going to be a really really really really really really long text.";
+            var summary = Control_Flow.StringUtility.SummarizeText(sentence, 40);
+            Console.WriteLine(summary);
+            
         }
+
+        
     }
 }
